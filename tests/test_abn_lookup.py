0a41cbd8cef4ff445cd66019800d506755b38552
Maksim Zinovev <maksimzinovevsubmit@gmail.com>
2021-03-28 19:14:13 +1100

    test_abn_details draft

--- tests/test_abn_lookup.py
+++ tests/test_abn_lookup.py
@@ -58,7 +58,7 @@ def test_abn_lookup(py):
     # GIVEN ABN lookup page has loade
     # WHEN user types search query into Search field
     # AND clicks search button
-    # THEN results should be relevant to search query
+    # THEN results contain expected results, such as ABN number, Name, Type, Location
 
     py.visit(BASE_URL)
     py.get(SEARCH_INPUT_FIELD).should().be_visible().type(SEARCH_QUERY)
@@ -74,3 +74,47 @@ def test_abn_lookup(py):
         assert found_row.contains(expected_result.name)
         assert found_row.contains(expected_result.type)
         assert expected_result.location in found_row.text()
+
+
+ExpectedAbnDetails = namedtuple("ExpectedAbnDetails",
+                                "entity_name abn_status entity_type business_location business_names")
+
+automic_pty_ptd_details = ExpectedAbnDetails(
+    entity_name="AUTOMIC PTY LTD",
+    abn_status="Active from 19 Jun 2012",
+    entity_type="Australian Private Company",
+    business_location="NSW 2000",
+    business_names=["CoSecPro", "Automic Group", "Invana", ]
+)
+
+ENTITY_TYPE = "//a[contains(text(), 'Australian Private Company')]"
+
+
+def test_abn_details(py):
+    """Verify search abn"""
+    # GIVEN ABN lookup page with the results has loaded
+    # WHEN user clicks on the ABN link
+    # THEN user is redirected to the Current details for ABN page
+    # AND ABN details match expected details
+
+    py.visit(BASE_URL)
+    py.get(SEARCH_INPUT_FIELD).should().be_visible().type(SEARCH_QUERY)
+    py.get(SEARCH_BUTTON).should().be_clickable().click()
+
+    found_row = py.contains(automic_pty_ptd.name).parent()
+    found_row.get("[href]").should().be_clickable().click()
+    assert py.url().endswith("27152260814")
+    assert "Current details for ABN 27 152 260 814" in py.get("h1").text()
+
+    assert automic_pty_ptd_details.entity_name in py.get("[itemprop='legalName']").text()
+    assert automic_pty_ptd_details.abn_status in py.getx('//th[contains(text(), "ABN status")]').parent().text()
+    assert automic_pty_ptd_details.entity_type in py.contains("Entity type:").parent().get("a").text()
+    assert automic_pty_ptd_details.business_location in py.get("[itemprop='addressLocality']").text()
+    for business_name in automic_pty_ptd_details.business_names:
+        assert business_name in py.contains("Business name(s)").parent().text()
+
+
+
+
+
+    time.sleep(2)
